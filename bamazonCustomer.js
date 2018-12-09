eabc93d2c2e9e8075d02c4a414def6899a1d9133
Ordolph <austin.t.randolph@gmail.com>
2018-12-08 22:12:58 -0500

    Added in node module to format products data into cli table, added inquirer prompts to buy items which updates stock quantity in database.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const mysql = require("mysql");
-const inuirer = require("inquirer");
+const inquirer = require("inquirer");
+const Table = require("cli-table");
 
 require('dotenv').config();
 
@@ -11,16 +12,55 @@ const connection = mysql.createConnection({
     database: 'bamazon_db'
 });
 
-connection.connect();
+let buildTable = function (res) {
+    var table = new Table({
+        head: ['Product ID', 'Product Name', 'Department', 'Price', 'Stock'],
+        // colWidths: [100, 100, 100, 100, 100],
+    })
 
-connection.query('SELECT * FROM products', function (err, res) {
-    if (err) {
-        console.log(err);
+    for (i = 0; i < res.length; i++) {
+        table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity])
     }
-    console.log(res);
+    // table.push(tableData);
+    console.log(table.toString());
+}
 
-    connection.destroy();
-});
+
+
+let customerStart = function () {
+
+    connection.query('SELECT * FROM products', function (err, res) {
+        if (err) {
+            console.log(err);
+        }
+        buildTable(res);
+
+        inquirer.prompt([{
+            type: 'input',
+            message: 'Please enter the id of the item you would like to buy.',
+            name: 'id',
+        }]).then(result => {
+            var id = result.id;
+
+            inquirer.prompt([{
+                type: 'input',
+                message: 'How many would you like to buy?',
+                name: 'quantity'
+            }]).then(result => {
+                var quantity = result.quantity;
+
+                connection.query(`UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?`, [quantity, id])
+                console.log(`You bought ${quantity}`)
+
+                customerStart();
+            })
+        })
+    })
+}
+
+customerStart();
+
+
 
 
 
